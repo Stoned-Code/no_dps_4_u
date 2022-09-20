56d5268d84a9063629cceb10baf2b88f42ba4751
Stoned-Code <animecomcommunity@gmail.com>
2022-09-19 17:06:47 -0700

    Added environment variables.

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import discord  # Import discord.py library.
 from discord.ext import commands  # Imports the commands script.
+import os
 
 import config  # Imports the config. (Create a "config.py" file with the TOKEN, PREFIX and PORT in it.)
 from bot_commands import register_bot_commands  # Imports the bot commands.
 from sc_libs.discord.command import SCCommand  # Imports the sc_libs Command class.
 
+TOKEN = os.environ['TOKEN']
 
 client = commands.Bot(command_prefix = config.PREFIX) # Creates discord bot client.
 SCCommand.set_prefix(config.PREFIX) # Sets the prefix for the help command info.
